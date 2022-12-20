cf66994897c3bf1142bdb390458a1428413b1933
sxtc2002 <2499948209@qq.com>
2022-12-20 14:31:25 +0800

    新增apng控制插件，优化派蒙动画，优化加载页代码

--- src/pages/Entrance.vue
+++ src/pages/Entrance.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="main-page" ref="flexible_fullscreen" class="main-page">
-      <img :src="`/assets/img/loading_bg_puzzle.png`" alt="loading_bg_puzzle" class="loading_bg_puzzle" />
-      <img :src="`/assets/img/loading_bg_di2.png`" alt="loading_bg_di2" class="loading_bg_di2" />
+    <img :src="`/assets/img/loading_bg_puzzle.png`" alt="loading_bg_puzzle" class="loading_bg_puzzle" />
+    <img :src="`/assets/img/loading_bg_di2.png`" alt="loading_bg_di2" class="loading_bg_di2" />
     <div v-show="pgy_show">
       <img :src="`/assets/img/loading_pgy2.png`" alt="loading_pgy2" class="loading_pgy2" />
       <img :src="`/assets/img/loading_pgy3.png`" alt="loading_pgy3" class="loading_pgy3" />
@@ -15,125 +15,78 @@
       <img :src="`/assets/img/minipgy/ball_2.png`" alt="ball_3" class="ball_3" />
     </div>
 
-    <img :src="pmUrl" id="pmgif" alt="logo" class="logo" v-show="pm_show"/>
-    <div class="title" v-show="!pm_show">须弥书</div>
-    <Btn v-show="(loaded===true)" class="loading_enter" @click="skip" type="Enter" content="点击进入" />
-    <div v-show="(loaded===false)" class="loading_percent">{{percent}}%</div>
+    <canvas ref="pm" alt="logo" class="logo" v-show="pm_show"></canvas>
+    <div class="title" v-show="title_show">须弥书</div>
+    <Btn v-show="(loaded === true)" class="loading_enter" @click="skip" type="Enter" content="点击进入" />
+    <div v-show="(loaded === false)" class="loading_percent">{{ percent }}%</div>
   </div>
 
 </template>
 
 <script setup lang="ts">
 import Btn from "../components/Btn.vue";
-import { floating_pgy, skip } from "../reactivity/entrance";
+import { skip } from "../reactivity/entrance";
 import { onMounted, Ref, ref, watch } from "vue";
 import { gsap } from "gsap";
+import { createApngPlayer } from "../reactivity/loadApng";
+import { loading_bg, loading_img } from "../reactivity/preload";
+import Player from "apng-js/types/library/player";
 
-const percent : Ref<number> = ref(0);
-const loaded : Ref<boolean> = ref(false);
-var pm_show : Ref<boolean> = ref(true);
-var pgy_show : Ref<boolean> = ref(false);
-var pmUrl : Ref<string> = ref("/assets/img/pm1.gif");
-var preimgUrl : string[] = [
-  "/assets/img/loading_bg_puzzle.png",
-  "/assets/img/loading_bg_di2.png",
-  "/assets/img/pm1.gif",
-  "/assets/img/pm2.gif",
-  "/assets/img/pm3.gif",
-];
-let preloadedNum = 0;
-for(let item of preimgUrl){
-  let img = new Image();
-  img.src = item;
-  img.onload = function(){
-    preloadedNum++;
-    if(preloadedNum == 5){
-      pm_show.value = true;
-      setTimeout(() => {
-        pmUrl.value = "/assets/img/pm2.gif";
-      }, 1200);
-    }
-  }
-}
+const percent: Ref<number> = ref(0);
+const loaded: Ref<boolean> = ref(false);
+const pm = ref<HTMLCanvasElement>();
+var pm_show: Ref<boolean> = ref(false);
+var pgy_show: Ref<boolean> = ref(false);
+var title_show: Ref<boolean> = ref(false);
+var pm1: Player, pm2: Player, pm3: Player;
 
-var pgyimgUrl : string[] = [
-  "/assets/img/loading_pgy2.png",
-  "/assets/img/loading_pgy3.png",
-  "/assets/img/loading_pgy4.png",
-  "/assets/img/minipgy/loading_minipgy.png",
-  "/assets/img/minipgy/loading_minipgyball.png",
-  "/assets/img/minipgy/ball_2.png",
-  "/assets/img/minipgy/mini_2.png",
-  "/assets/img/minipgy/ball_2.png",
-];
+loading_bg(pgy_show);
+loading_img(percent, loaded);
 
-let pgyloadedNum = 0;
-let pgyNum = pgyimgUrl.length;
-for(let item of pgyimgUrl){
-  let img = new Image();
-  img.src = item;
-  img.onload = function(){
-    pgyloadedNum++;
-    if(pgyloadedNum == pgyNum){
-      pgy_show.value = true;
-      floating_pgy();
-    }
-  }
-}
-
-setTimeout(() => {
-  pmUrl.value = "/assets/img/pm2.gif";
-}, 1200);
-
-watch(loaded, (newloaded) => {
-  pmUrl.value = "/assets/img/pm3.gif";
-  setTimeout(() => {
-    pm_show.value = false;
-  }, 840);
-})
-
-const imgModule = import.meta.glob('../../assets/**/*.{png,svg,gif,jpg}',{as:'url'});
-//console.log(imgModule);
-const imgUrl : string[] = [];
-Object.keys(imgModule).forEach(item =>{
-  imgUrl.push(item);
+onMounted(async () => {
+  if (pm != null) {
+    var canvas = pm.value as HTMLCanvasElement;
+    canvas.width = 545;
+    canvas.height = 545;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    pm1 = await createApngPlayer("/assets/img/pm1.png", ctx, true);
+    pm2 = await createApngPlayer("/assets/img/pm2.png", ctx, false);
+    pm3 = await createApngPlayer("/assets/img/pm3.png", ctx, true);
+    pm1.play();
+    pm_show.value = true;
+    pm1.on('end', () => {
+      pm2.play();
+    });
+    pm2.on('frame', () => {
+      if (pm2.currentFrameNumber == 5) {
+        if (loaded)
+          pm2.stop();
+      }
+    })
+    pm2.on('stop', () => {
+      pm3.play();
+    })
+    pm3.on('end', () => {
+      pm_show.value = false;
+      title_show.value = true;
+      gsap.fromTo(".title", {
+        opacity: 0.7,
+        scale: 0.9,
+      }, {
+        opacity: 1,
+        scale: 1,
+        duration: 0.5,
+      })
+    })
+  };
 });
-const imgNum = imgUrl.length;
-let loadedNum = 0;
 
-for(let item of imgUrl){
-  let img = new Image();
-  img.src = item;
-  img.onload = function(){
-    loadedNum++;
-    percent.value=Math.floor((loadedNum/imgNum)*100);
-    if(loadedNum===imgNum){
-      pmUrl.value = "/assets/img/pm3.gif";
-      setTimeout(() => {
-        pm_show.value = false;
-        loaded.value=true;
-        gsap.fromTo(".title", {
-          opacity: 0.7,
-          scale: 0.9,
-        }, {
-          opacity: 1,
-          scale: 1,
-          duration: 0.5,
-        })
-      }, 840);
-    }
-  }
-}
-
-onMounted(() => {
-  floating_pgy();
-});
 
 </script>
 
 <style lang="scss" scoped>
 .main-page {
-  .loading_percent{
+  .loading_percent {
     position: absolute;
     left: 0;
     right: 0;
@@ -147,6 +100,7 @@ onMounted(() => {
     text-align: center;
     z-index: 4;
   }
+
   .loading_bg_puzzle {
     z-index: 1;
     position: absolute;
@@ -248,6 +202,7 @@ onMounted(() => {
     left: 0;
     right: 0;
     text-align: center;
+    height: calc(1/5*var(--width));
     width: calc(1/5*var(--width));
     top: calc((45/100*var(--height)) / 2);
     z-index: 4;
